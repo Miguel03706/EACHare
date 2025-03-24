3a26e7e0dffa1a8d19e2c531f2d69c88bf4aaabe
Miguel <75583778+Miguel03706@users.noreply.github.com>
2025-03-24 16:15:08 -0300

    criando arquivo server

--- socket/server.py
+++ socket/server.py
@@ -7,13 +7,30 @@ SERVER_PORT = 5050
 SERVER_ADDR = (SERVER_IP, SERVER_PORT)
 FORMATO = 'utf-8'
 
+conections = []
+messages = []
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(SERVER_ADDR)
 
+
+def sendMessageIndividual(conn):
+    global messages
+    for msg in messages:
+        conn.send(msg.encode(FORMATO))
+        time.sleep(0.5)
+
+def sendMessageToAll(msg):
+    global conections
+    for conn in conections:
+        sendMessageIndividual(conn)
+
+# Envia mensagem para todos os clientes conectados
 def handleClient(conn, addr):
     print(f"[Nova conexão] Usuário: {addr} conectado. ")
     print(f"[Conexão] : {conn}")
     global conections
+    global messages
 
     while True:
         msg = conn.recv(1024).decode(FORMATO)
@@ -23,9 +40,12 @@ def handleClient(conn, addr):
         if (msg.startswith("MSG=")):
             msg_split = msg.split("=")
             msg_final = msg_split[1]
+            mensages.append(msg_final)
+            sendMessageToAll(msg_final)
 
             conections.append(msg_final)
 
+# Inicia o servidor em threads
 def startServer():
     print("[Iniciando] servidor")
     server.listen()
